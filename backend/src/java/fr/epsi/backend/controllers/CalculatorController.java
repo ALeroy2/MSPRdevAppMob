e1e682b30989190c517bf8187a29e4f50d1c41d1
Laplijr <ronanlaplaud4@gmail.com>
2019-04-25 11:46:09 +0200

    calculator controller & calculator service

--- backend/src/java/fr/epsi/backend/controllers/CalculatorController.java
+++ backend/src/java/fr/epsi/backend/controllers/CalculatorController.java
@@ -1,3 +1,26 @@
-public class CalculatorController {
+package java.fr.epsi.backend.controllers;
 
+import java.fr.epsi.backend.services.CalculatorService;
+
+public class CalculatorController {
+    private CalculatorService calculatorService;
+
+    public CalculatorController() {
+        this.calculatorService = new CalculatorService();
+    }
+    public float getDiscountRate(float net, float brut) {
+        return this.calculatorService.getDiscountRate(net, brut);
+    }
+
+    public float getNetBuyingPrice(float brut, float discountRate) {
+        return this.calculatorService.getNetBuyingPrice(brut, discountRate);
+    }
+
+    public float getNetSellingPrice(float netBuyingPrice, float multiplicationCoeff) {
+        return this.calculatorService.getNetSellingPrice(netBuyingPrice, multiplicationCoeff);
+    }
+
+    public float getMultiplicationCoeff(float netSellingPrice, float netBuyingPrice) {
+        return this.calculatorService.getMultiplicationCoeff(netSellingPrice, netBuyingPrice);
+    }
 }
---
+++ backend/src/java/fr/epsi/backend/services/CalculatorService.java
@@ -0,0 +1,23 @@
+package java.fr.epsi.backend.services;
+
+public class CalculatorService {
+
+    public CalculatorService() {
+    }
+
+    public float getDiscountRate(float net, float brut) {
+        return (1 - net / brut) * 100;
+    }
+
+    public float getNetBuyingPrice(float brut, float discountRate) {
+        return brut * (1 - discountRate);
+    }
+
+    public float getNetSellingPrice(float netBuyingPrice, float multiplicationCoeff) {
+        return netBuyingPrice * multiplicationCoeff;
+    }
+
+    public float getMultiplicationCoeff(float netSellingPrice, float netBuyingPrice) {
+        return netSellingPrice / netBuyingPrice;
+    }
+}
