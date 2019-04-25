f11ab09cf0ceec31b8c41819a47816a7435c3323
Houlgatte Louka <lhoulgattebustamante@gmail.com>
2019-04-25 11:35:36 +0200

    managed first component Calculator

---
+++ frontendweb/src/containers/Calculator/Calculator.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+
+  render() {
+    return (
+      <div className="container">
+        <div>Flex1</div>
+        <div>Flex2</div>
+        <div>Flex3</div>
+        <div>Flex4</div>
+      </div>
+    )
+  }
+}
+
+export default Calculator;
