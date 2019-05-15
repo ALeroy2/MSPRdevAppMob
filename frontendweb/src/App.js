657b271da0a153dc4a2b072a64b011f2e09fca06
Julien Auneau <jauneau@MacBook-Pro-de-Julien-2.local>
2019-05-15 10:57:41 +0200

    implantation des boutons + modals

--- frontendweb/src/App.js
+++ frontendweb/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Calculator from "./containers/Calculator/Calculator";
+import 'materialize-css/dist/css/materialize.min.css';
 
 function App() {
   return (
--- frontendweb/src/containers/Calculator/Calculator.js
+++ frontendweb/src/containers/Calculator/Calculator.js
@@ -1,15 +1,95 @@
 import React from 'react';
-
+import { CardPanel,Row,Col,Container } from 'react-materialize';
+import M from "materialize-css";
 class Calculator extends React.Component {
+    componentDidMount() {
+        // Auto initialize all the things!
+        M.AutoInit();
+    }
+
 
   render() {
     return (
-      <div className="container">
-        <div>Flex1</div>
-        <div>Flex2</div>
-        <div>Flex3</div>
-        <div>Flex4</div>
-      </div>
+
+    <div className="container" style={{textAlign: 'center',  paddingRight: 10}}>
+
+    <Row>
+        <CardPanel id={"calculTauxRemise"} href={"#modal1"} className="col s3 deep-purple accent-1 modal-trigger" style={{height: 150, width: 200}}>
+            <span className="white-text flow-text" >
+                Taux de remise
+            </span>
+        </CardPanel>
+        <div id="modal1" className="modal">
+            <div className="modal-content">
+                <h4>Taux de remise</h4>
+
+                <div className="row">
+                    <form className="col s12" onSubmit={""}>
+                        <div className="row">
+                            <div className="input-field col s6">
+                                <input id="prixAchatNet1" type="text" className="validate"/>
+                                <label htmlFor="dataOne">Prix d'achat net</label>
+                            </div>
+                            <div className="input-field col s6">
+                                <input id="prixAchatBrut1" type="text" className="validate"/>
+                                <label  htmlFor="dataTwo">Prix d'achat brut</label>
+                            </div>
+                        </div>
+                        <button type={"submit"} className={"btn"}>Calculer</button>
+                    </form>
+                </div>
+            </div>
+            <div className="modal-footer">
+                <a href="#!" className="modal-close waves-effect waves-green btn-flat">Fermer</a>
+            </div>
+        </div>
+        <CardPanel className="col s3 deep-purple accent-2" href={"#modal2"} style={{height: 150, width: 200, marginLeft: 50}}>
+          <span className="white-text flow-text">
+                Prix d'achat
+          </span>
+        </CardPanel>
+        <div id="modal2" className="modal">
+            <div className="modal-content">
+                <h4>Prix d'achat</h4>
+
+                <div className="row">
+                    <form className="col s12" onSubmit={""}>
+                        <div className="row">
+                            <div className="input-field col s6">
+                                <input id="tauxRemise2" type="text" className="validate"/>
+                                <label htmlFor="tauxRemise2">Taux de remise</label>
+                            </div>
+                            <div className="input-field col s6">
+                                <input id="prixBrut2" type="text" className="validate"/>
+                                <label  htmlFor="prixBrut2">Prix d'achat brut</label>
+                            </div>
+                        </div>
+                        <button type={"submit"} className={"btn"}>Calculer</button>
+                    </form>
+                </div>
+            </div>
+            <div className="modal-footer">
+                <a href="#!" className="modal-close waves-effect waves-green btn-flat">Fermer</a>
+            </div>
+        </div>
+    </Row>
+    <Row>
+        <CardPanel className="col s3 deep-purple accent-3" style={{height: 150, width: 200}}>
+          <span className="white-text flow-text">
+                Prix de vente net
+          </span>
+        </CardPanel>
+        <CardPanel className="col s3 deep-purple accent-4" style={{height: 150, width: 200, marginLeft: 50 }}>
+          <span className="white-text flow-text">
+                Coefficient multiplicateur
+          </span>
+        </CardPanel>
+    </Row>
+
+    </div>
+
+
+
     )
   }
 }
